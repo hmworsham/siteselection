55a8f6d6f28b8b6f29f08c2b6bc3d6df857a32ab
hmworsham <hmworsham@gmail.com>
2023-01-19 14:35:27 -0700

    added lines to calculate zonals for new sapflux and aspen sites

--- EastRiver_Derive_Topos.R
+++ EastRiver_Derive_Topos.R
@@ -39,6 +39,7 @@ wsdir <- file.path(erdir, 'Working_Files', 'Watershed_Spatial_Dataset', 'Output'
 rasdir <- file.path(erdir, 'RMBL-East River Watershed Forest Data', 'Data', 'Geospatial', 'Worsham_SiteSelection', '2021_Analysis_Layers', 'USGS_1-9_arcsec_DEM')
 sfdir <- file.path(erdir, 'RMBL-East River Watershed Forest Data', 'Data', 'Geospatial')
 potrdir <- file.path(erdir, 'RMBL-East River Watershed Forest Data', 'Data', 'Geospatial', 'Blonder_Aspen_Plots_2020')
+berkdir <- file.path(erdir, 'RMBL-East River Watershed Forest Data', 'Data', 'Geospatial', 'Berkelhammer_Still_Sapflux_Sites_2021')
 
 #############################
 # Ingest data
@@ -69,19 +70,69 @@ names(coords.22)[1] <- 'Location_ID'
 plots.ext <- list.files(file.path(sfdir, 'Kueppers_EastRiver_Plot_Shapefiles_WGS84UTM13N', 'AllPlots'), pattern = 'shp', full.names = T)
 
 # Ingest cored aspen coordinates
-aspen <- st_read(list.files(potrdir, full.names=T)[5])
-aspen.coords <- data.frame(Tree_Code=aspen$Tree_Code, Longitude=aspen$Longitude, Latitude=aspen$Latitude)
+aspen <- read.csv(file.path(potrdir, 'aspen data site-level processed 30 Mar 2020.csv'))
+View(aspen)
+aspen.coords <- data.frame(Site_Code=aspen$Site_Code, Longitude=aspen$Longitude, Latitude=aspen$Latitude)
 aspen.coords <- na.omit(aspen.coords)
+
+# Ingest Berkelhammer coordinates
+berk <- read.csv(file.path(berkdir, 'SapSites.csv'))
+berk.coords <- berk %>%
+  group_by(site..) %>%
+  filter(row_number()==1)
+berk.coords <- data.frame(Location_ID=berk.coords$site.., Longitude=berk.coords$longitude, Latitude=berk.coords$latitude)
 
 #############################
 # Compute topographic stats
 #############################
 
-# Compute topo statistics for new plots
-zonnew <- zonals(aspen.coords, rasdir, topo.factors, type = 'coord', radius = 5)
+# Compute topo statistics for Kueppers plots
+zon.kplots <- zonals(coords.ext, rasdir, topo.factors, type='coord', radius=20)
 
 # Write out topo stats to csv
-write.csv(zonnew, '~/Desktop/EastRiver_Proposed_Sites_22_10.csv')
+write.csv(zon.kplots, '~/Desktop/kueppers_plots_zonals.csv')
+
+# Compute topo statistics for aspen plots
+zon.asp <- zonals(aspen.coords, rasdir, topo.factors, type = 'coord', radius = 10)
+aspen.coords[aspsites,]
+aspsites <- which(aspen.coords$Site_Code %in% c('ACGNT01',
+                                                'AGXLZ',
+                                                'DBUZY',
+                                                'FOASS01',
+                                                'GRJXK',
+                                                'NGTWF',
+                                                'OFCNI',
+                                                'POZDQ',
+                                                'WCPJH',
+                                                'WPRLZ',
+                                                'XKPJS',
+                                                'ZJQRW'))
+
+aspsites
+zon.asp <- zon.asp[aspsites,]
+
+# Write out topo stats to csv
+write.csv(zon.asp, '~/Desktop/aspen_zonals.csv')
+
+# Compute zonals for Berkelhammer sapflux sites
+zon.berk <- zonals(berk.coords, rasdir, topo.factors, type='coord', radius=20)
+
+# Write out topo stats to csv
+write.csv(zon.berk, '~/Desktop/berkelhammer_zonals.csv')
+
+# Potential new sap flux sites
+p1 <- c(-106.9744584,38.9271352)
+p2 <- c(-106.983025,38.924723)
+p3 <- c(-107.0071540,38.9736823)
+lons <- c(p1,p2,p3)[c(TRUE, FALSE)]
+lats <- c(p1,p2,p3)[c(FALSE,TRUE)]
+newsf.coords <- data.frame('ID'=seq(length(lons)),
+                            'Longitude'=lons,
+                            'Latitude'=lats)
+
+
+zon.newsf <- zonals(newsf.coords, rasdir, topo.factors, type='coord', radius=5)
+write.csv(zon.newsf, '~/Desktop/newsf_zonals.csv')
 
 ########################################
 # Prepare coordinates to submit for review
