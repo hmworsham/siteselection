66b1da8c51f846828cdc81e1884ae0dd244fdbd3
hmworsham <hmworsham@gmail.com>
2023-02-05 15:50:59 -0700

    use config$extdata for data ingest

--- inst/notebooks/02.03_derive_topos_aspen_2023.R
+++ inst/notebooks/02.03_derive_topos_aspen_2023.R
@@ -1,18 +1,8 @@
-# EastRiver_Derive_Topos
-# Calculates the values of various topographic factors for rectangular 40x40 m neighborhoods around specified coordinates and/or within the extents of forest inventory plots in the East River watershed.
+# Derive topo values for Blonder aspen plots
+# Calculates the values of various topographic factors for rectangular 40x40 m neighborhoods around specified coordinates and/or within the extents of aspen plots in the East River watershed.
 # Author: Marshall Worsham
-# Created: 10-06-20
-# Revised: 02-02-23
-
-# List packages
-pkgs <- c('data.table',
-          'dplyr',
-          'ggplot2',
-          'googledrive',
-          'readxl',
-          'sf',
-          'terra',
-          'tidyverse')
+# Created: 01-20-23
+# Revised: 02-03-23
 
 # Load config
 config <- config::get(file=file.path('config', 'config.yml'))
@@ -21,167 +11,72 @@ config <- config::get(file=file.path('config', 'config.yml'))
 devtools::load_all()
 load.pkgs(config$pkgs)
 
+# Ingest aspen site info
+asp <- drive_download(as_id(config$asp), tempfile())$local_path
+asp <- read.csv(asp)
+head(asp)
 
-#
-topo.factors <- c('Aspect',
-                  'Curvature',
-                  'DTM',
-                  'DEM',
-                  'Heat_Load',
-                  'Slope',
-                  'TPI',
-                  'TWI')
+# Ingest cored aspen info
+asp.cored <- drive_download(as_id(config$aspcored), tempfile())$local_path
+asp.cored <- read.csv(asp.cored)
+head(asp.cored)
 
-# Ingest 2020 Kueppers plot characteristics CSVs
-tmpfile <- tempfile()
-tmpfile <- drive_download('Kueppers_EastRiver_Site_Index', tmpfile)$local_path
-siteinfo.ext <- read_excel(tmpfile)
-coords.ext <- siteinfo.ext[,c('Location_ID', 'Latitude', 'Longitude')]
+#### Process and clean data ####
 
-# Ingest 2022 proposed site info
-siteinfo.22 <- read.csv(file.path(fidir, 'EastRiver_Proposed_Sites_2022_10.csv'))
-
-# Make df of location names and coordinates
-coords.22 <- siteinfo.22[,c('Location_Name', 'Latitude', 'Longitude')]
-names(coords.22)[1] <- 'Location_ID'
-
-# Ingest boundaries of existing plots
-plots.ext <- list.files(file.path(sfdir, 'Kueppers_EastRiver_Plot_Shapefiles_WGS84UTM13N', 'AllPlots'), pattern = 'shp', full.names = T)
-
-# Ingest cored aspen coordinates
-aspen <- read.csv(file.path(potrdir, 'aspen data site-level processed 30 Mar 2020.csv'))
-aspen.cored <- read.csv(file.path(potrdir, 'aspen dendro 2021 - completed sites.csv'))
-aspen <- merge(aspen, aspen.cored, by='Site_Code', all=T)
+# Merge site info and cored tree info
+aspen <- merge(asp, asp.cored, by='Site_Code', all=T)
 aspen <- aspen[!is.na(aspen$Date.Completed),]
-aspen.coords <- data.frame(Site_Code=aspen$Site_Code, Longitude=aspen$Longitude, Latitude=aspen$Latitude)
+aspen.coords <- data.frame(Site_Code=aspen$Site_Code,
+                           Longitude=aspen$Longitude,
+                           Latitude=aspen$Latitude)
 aspen.coords <- na.omit(aspen.coords)
 
-# Ingest Berkelhammer coordinates
-berk <- read.csv(file.path(berkdir, 'SapSites.csv'))
-berk.coords <- berk %>%
-  group_by(site..) %>%
-  filter(row_number()==1)
-berk.coords <- data.frame(Location_ID=berk.coords$site.., Longitude=berk.coords$longitude, Latitude=berk.coords$latitude)
+# Filter for target sites for sap flux install in 2023
+targ <- asp[which(asp$Site_Code %in% c('ACGNT01',
+                                   'AGXLZ',
+                                   'DBUZY',
+                                   'FOASS01',
+                                   'GRJXK',
+                                   'NGTWF',
+                                   'OFCNI',
+                                   'POZDQ',
+                                   'WCPJH',
+                                   'WPRLZ',
+                                   'XKPJS',
+                                   'ZJQRW')),]
 
-#############################
-# Compute topographic stats
-#############################
+targ.coords <- data.frame(Site_Code=targ$Site_Code,
+                          Longitude=targ$Longitude,
+                          Latitude=targ$Latitude)
 
-# Compute topo statistics for Kueppers plots
-zon.kplots <- zonals(coords.ext, rasdir, topo.factors, type='coord', radius=20)
+#### Compute topographic metrics ####
 
-# Write out topo stats to csv
-write.csv(zon.kplots, '~/Desktop/kueppers_plots_zonals.csv')
+# Specify topographic factors of interest
+topo.factors <- c('dem_100m',
+                  '205f_aspect_100m',
+                  'aspect_100m',
+                  'curvature_100m',
+                  'heatload_100m',
+                  'slope_100m',
+                  'tpi_1000m',
+                  'twi_100m',
+                  'twi_1000m')
+
+# List raster files corresponding to factors of interest
+topo.rasters <- list.files(config$dat_int,
+                           paste0('*', topo.factors, '.tif', collapse='|'),
+                           full.names=T)
+
+topo.rasters <- lapply(topo.rasters, rast)
 
 # Compute topo statistics for all cored aspen plots
-zon.asp <- zonals(aspen.coords, rasdir, topo.factors, type='coord', radius=10)
-
-# Compute topo statistics for target SF aspen plots
-zon.asp <- zonals(aspen.coords, rasdir, topo.factors, type = 'coord', radius = 10)
-aspen.coords[aspsites,]
-aspsites <- which(aspen.coords$Site_Code %in% c('ACGNT01',
-                                                'AGXLZ',
-                                                'DBUZY',
-                                                'FOASS01',
-                                                'GRJXK',
-                                                'NGTWF',
-                                                'OFCNI',
-                                                'POZDQ',
-                                                'WCPJH',
-                                                'WPRLZ',
-                                                'XKPJS',
-                                                'ZJQRW'))
-
-aspsites
-zon.asp <- zon.asp[aspsites,]
+aspen.zonals <- zonals(aspen.coords, topo.rasters, type='coord', radius=10, shape='circle')
 
 # Write out topo stats to csv
-write.csv(zon.asp, '~/Desktop/aspen_zonals.csv')
+write.csv(aspen.zonals, file.path(config$dat_pro, 'aspen_zonals_2023.csv'))
 
-# Compute zonals for Berkelhammer sapflux sites
-zon.berk <- zonals(berk.coords, rasdir, topo.factors, type='coord', radius=20)
+# Compute topo statistics for targeted sapflux plots
+targ.zonals <- zonals(targ.coords, topo.rasters, type='coord', radius=10, shape='circle')
 
 # Write out topo stats to csv
-write.csv(zon.berk, '~/Desktop/berkelhammer_zonals.csv')
-
-# Potential new sap flux sites
-p1 <- c(-106.9744584,38.9271352)
-p2 <- c(-106.983025,38.924723)
-p3 <- c(-107.0071540,38.9736823)
-lons <- c(p1,p2,p3)[c(TRUE, FALSE)]
-lats <- c(p1,p2,p3)[c(FALSE,TRUE)]
-newsf.coords <- data.frame('ID'=seq(length(lons)),
-                            'Longitude'=lons,
-                            'Latitude'=lats)
-
-
-zon.newsf <- zonals(newsf.coords, rasdir, topo.factors, type='coord', radius=5)
-write.csv(zon.newsf, '~/Desktop/newsf_zonals.csv')
-
-########################################
-# Prepare coordinates to submit for review
-########################################
-
-# Bind existing coordinates df to new coordinates df
-allcoords <- rbind(coords.ext, coords.22)
-
-# Create sets of 'good' and 'bad' sites for inclusion/exclusion
-# goodsites <- c() # Use if good sites list is short
-badsites <- c('Ute Gulch 2',
-              'Baldy Mountain east 4',
-              'Snodgrass NW slope 2',
-              'Emerald 1',
-              'Cement Creek 28')
-goodsites <- coords.22[!coords.22$Location_ID %in% badsites, 'Location_ID']
-
-# Filter all coordinates to those in goodsites
-submitcoords <- allcoords[allcoords$Location_ID %in% goodsites,]
-
-# Rename rows to integer sequence
-rownames(submitcoords) <- seq(1, nrow(submitcoords))
-
-# Write coordinates to CSV
-write.csv(
-  submitcoords,
-  file.path(
-    fidir,
-    'EastRiver_Proposed_Coordinates_2022_10.csv')
-  )
-
-#######################################################
-# Create hypothetical plot polygons and write to shp
-#######################################################
-
-# Make polygons from submission coordinates
-submitpolys <- makepolys(submitcoords, radius=20, shape='rectangle')
-submitpolys.sf <- st_as_sf(submitpolys)
-submitpolys.sf$Location_ID <- goodsites
-
-# Write polygons to shp
-st_write(
-  submitpolys,
-  file.path(sfdir,
-            'Worsham_2021_SiteSelection',
-            '2022_Proposed_Sites',
-            'Kueppers_EastRiver_Proposed_Sites_2022_10'),
-  driver="ESRI Shapefile", append=F)
-
-# Check that shp is readable and plot
-plot(
-  rast(
-    list.files(
-      rasdir,
-      recursive=T,
-      full.names=T)[12]),
-  col=gray.colors(12))
-
-plot(st_read(
-  file.path(
-    sfdir,
-    'Worsham_2021_SiteSelection',
-    '2022_Proposed_Sites',
-    'Kueppers_EastRiver_Proposed_Sites_2022_10')),
-  lwd=6,
-  border=magma(10),
-  add=T)
-
+write.csv(targ.zonals, file.path(config$dat_pro, 'aspen_sf_proposed_zonals_2023.csv'))
