c83f7e13dd9342403b2ad3d910778920c3ba7b4c
hmworsham <hmworsham@gmail.com>
2023-02-06 17:43:18 -0700

    refactor to use new print.figs()

--- inst/notebooks/03_plot_gradients_conifer_fullnetwork.R
+++ inst/notebooks/03_plot_gradients_conifer_fullnetwork.R
@@ -13,35 +13,28 @@ config <- config::get(file=file.path('config', 'config.yml'))
 devtools::load_all()
 load.pkgs(config$pkgs)
 
-# Name packages to install
-pkgs <- c(
-  'data.table',
-  'dplyr',
-  'ggplot2',
-  'ggpubr',
-  'googledrive',
-  'readxl',
-  'tidyverse',
-  'mrmoose')
-
 # Ingest 2020 Kueppers plot characteristics CSVs
-tmpfile <- drive_download(as_id(config$extdata$siteindex, tempfile()))$local_path
+tmpfile <- drive_download(as_id(config$extdata$siteindex), tempfile())$local_path
 siteinfo <- read_excel(tmpfile)
 
 # Select variables of interest from 2021 site info
-topos <- siteinfo[c('Location_ID',
-                  'Established',
-                  'Within_SDP_Boundary',
-                  'Elevation_m',
-                  'Slope',
-                  'Aspect',
-                  'Heat_Load',
-                  'Folded_Aspect_205',
-                  'Southness_205',
-                  'TWI_100',
-                  'TWI_1000',
-                  'TPI_1000',
-                  'TPI_2000')]
+topos <- siteinfo[c(
+  'Location_ID',
+  'Established',
+  'Within_SDP_Boundary',
+  'Sensors',
+  'Cored',
+  'Elevation_m',
+  'Slope',
+  'Aspect',
+  'Heat_Load',
+  'Folded_Aspect_205',
+  'Southness_205',
+  'TWI_100',
+  'TWI_1000',
+  'TPI_1000',
+  'TPI_2000'
+)]
 
 # Make df of topo variables
 topos <- as.data.frame(topos)
@@ -51,67 +44,42 @@ topos <- as.data.frame(topos)
 # topos <- topos[!topos$Site_ID %in% outs, ]
 # topos <- topos[topos$Established == 'Established', ]
 
-# Add column for sensor presence
-topos$Sensor <- 'Sensors Absent'
-
-# Assign T for sites with sensors installed
-topos[topos$Location_ID %in% c(
-  'CC-CVS1',
-  'ER-APL1',
-  'ER-APU1',
-  'ER-BME1',
-  'ER-GT1',
-  'SG-NES1',
-  'SG-SWR1',
-  'SR-PVG1',
-  'XX-CAR3',
-  'XX-PLN1'
-), 'Sensor'] <- 'Sensors Present'
-
-# Add column for cored sites
-topos$Coring <- 'Not Cored'
-topos[topos$Location_ID %in% c('CC-CVS1',
-                                       'CC-UC1',
-                                       'ER-APL1',
-                                       'ER-APU1',
-                                       'ER-BME1',
-                                       'ER-GT1',
-                                       'SG-NES1',
-                                       'SG-NES2',
-                                       'SG-SWR1',
-                                       'SR-PVG1',
-                                       'XX-CAR1',
-                                       'XX-CAR2',
-                                       'XX-CAR3',
-                                       'XX-PLN1',
-                                       'XX-PLN2'), 'Coring'] <- 'Cored'
-
-topos[topos$Location_ID %in% c('CC-CVS1',
-                                       'ER-APU1',
-                                       'SG-NES1',
-                                       'SR-PVG1',
-                                       'CC-EMN1'), 'Coring'] <- 'Proposed'
-
 # Subset only established sites
 # topos_est <- topos[topos$Established == T]
 
-# Subset only proposed coring sites
+# Subset only cored sites
 # topos_coring <- topos[topos$Coring=='Cored',]
 
 # Subset only plots in aop domain
 # topos_aop <- topos[topos$Within_SDP_Boundary=='Yes',]
 
-##############################################
-# Batch create plots for individual variables
-##############################################
+#### Batch create plots for individual variables ####
+
+
+# Define colors
+colors = c("#3B9AB2",
+           "#E1AF00",
+           "#F21A00",
+           "#78B7C5",
+           "#00A08A",
+           "#F98400",
+           "#4DA64D",
+           "#DFB3F2",
+           "#EBCC2A",
+           "#2A2A2A")
 
 # Print figures to png
-outdir = file.path(fidir, 'Production_Images', 'AOP_Sites')
-print.figs(topos_aop, outdir)
+out.dir <- file.path('inst', 'reports', 'figs')
+print.figs(
+  topos,
+  'Sensors',
+  pal=colors,
+  outdir=out.dir,
+  dims=c(2560,1920),
+  reso=300)
 
-############################
-# Facet grid all variables
-############################
+
+#### Facet grid all variables ####
 
 # Specify vars to include
 topos.no <- c('Aspect',
@@ -121,39 +89,53 @@ topos.no <- c('Aspect',
 kplots <- topos[,!names(topos) %in% topos.no]
 
 # Gather to long format
-kplots_long <- gather(
-  kplots,
-  variable,
-  value, -c(
-    Location_ID,
-    Established,
-    Within_SDP_Boundary,
-    Sensor,
-    Coring))
-kplots_long$Elevation <- kplots_long[kplots_long$variable=='Elevation_m','value']
+kplots_long <- kplots %>%
+  pivot_longer(cols = Elevation_m:TPI_1000,
+               names_to = 'variable') %>%
+  arrange(variable, Location_ID)
 kplots_long <- kplots_long %>%
+  mutate(Elevation = as.numeric(
+    flatten(
+      rep(kplots_long[kplots_long$variable == 'Elevation_m', 'value'],
+          length(unique(kplots_long$variable))
+  )))) %>%
   arrange(variable, value) %>%
-  mutate(order = row_number())
+  group_by(variable) %>%
+  mutate(Order = 1:25) %>%
+  ungroup()
 
 # Set up facet grid with all variables
-varsgrid <- ggplot(kplots_long, aes(x = order, y = value)) +
+varsgrid <- ggplot(kplots_long, aes(x = Order, y = value)) +
   geom_point(aes(fill = Elevation, size=1), shape=21) +
-  #scale_fill_viridis_c(name='Elevation') +
-  #scale_fill_manual(values=unname(icolors('mario'))) +
-  scale_alpha_discrete(range = c(1, 0.2)) +
+  scale_fill_viridis_c(name='Elevation') +
+  #scale_alpha_discrete(range = c(1, 0.2)) +
   scale_x_continuous(
-    breaks = kplots_long$order,
+    breaks = kplots_long$Order,
     labels = kplots_long$Location_ID) +
-  geom_smooth(method = "lm", se=FALSE, color='black') +
-  stat_poly_eq(
-    coef.digits=3,
-    use_label(c('eq', 'R2'))) +
+  geom_smooth(method = 'lm', se=FALSE, color='black') +
+  stat_poly_eq(aes(label = paste(after_stat(eq.label), sep = "~~~")),
+               label.x.npc = "right",
+               label.y.npc = 0.15,
+               eq.with.lhs = "italic(hat(y))~`=`~",
+               eq.x.rhs = "~italic(x)",
+               formula = y~x ,
+               parse = TRUE,
+               size = 3) +
+  stat_poly_eq(aes(label = paste(after_stat(rr.label), sep = "~~~")),
+               label.x.npc = "right",
+               label.y.npc = "bottom",
+               formula = y~x,
+               parse = TRUE,
+               size = 3) +
   facet_wrap(~variable, scales = 'free') +
-  guides(color='none', fill='none', size='none') +
   labs(x='Site', y='Value') +
+  guides(color='none', fill='none', size='none') +
   theme(
-    axis.text.x = element_text(size=9, angle=90, hjust=0.95, vjust=0.2),
+    axis.text.x = element_text(size=9, angle=90, hjust=1, vjust=0.5),
     axis.title = element_text())
 
 # Print facet grid
 varsgrid
+png(file.path(out.dir, 'topo_fg.png'))
+print(varsgrid)
+dev.off()
