56a1a2e2a0d1e1b10d6de5d144dbdd2c639997f0
hmworsham <hmworsham@gmail.com>
2023-02-06 17:43:08 -0700

    refactor print.figs

--- R/helpers.R
+++ R/helpers.R
@@ -28,106 +28,116 @@ makepolys <- function(input, radius, shape='rectangle', target.crs=32613){
 #' @param outdir Character string. Destination directory for writing png
 #' @return NULL. Writes to file
 
-print.figs <- function(df, outdir){
+print.figs <- function(df,
+                        factor.var,
+                        pal=NULL,
+                        col.on=NULL,
+                        outdir,
+                        dims=c(1280,900),
+                        reso=300){
 
-  # Define colors
-  colors = c("#3B9AB2",
-             "#E1AF00",
-             "#F21A00",
-             "#78B7C5",
-             "#00A08A",
-             "#F98400",
-             "#4DA64D",
-             "#DFB3F2",
-             "#EBCC2A",
-             "#2A2A2A")
+  # Create outdir if not exists
+  dir.create(file.path('.', outdir), showWarnings = FALSE)
 
-  varnames = c('Elevation (m)',
-               'Slope angle (º)',
-               'Aspect (º)',
-               expression(Heat~Load~(MJ~cm^-2~y^-1)),
-               'Aspect Folded on 205º (º)',
-               'Southness Adjusted to 205º (º)',
-               'TWI [100 m]',
-               'TWI [1000 m]',
-               'TPI [1000 m]',
-               'TPI [2000 m]')
+  # Set topo variables
+  df <- data.frame(
+    Location_ID=df$Location_ID,
+    Factor_Var=df[,factor.var],
+    df %>% select_if(is.numeric))
 
-  # Refactor Established
-  df$Established <- factor(df$Established, levels = c('Established', 'Approved', 'Proposed'))
+  # Set factor column as factor and set name
+  df$Factor_Var <- factor(df$Factor_Var, levels=c('T','F'))
+  #names(df)[names(df)=='Factor_Var'] <- factor.var
 
-  # Loop through variables
-  for (t in seq(3,length(df))){
-    clr = colors[t-2]
-    varname = varnames[t-2]
+  # lapply
+  for (t in seq(3, length(df))) {
 
-    #LM coeff
-    lmv = df[order(df[, t]), t]
-    lmi = round(coefficients(lm(lmv ~ seq(1, nrow(df))))[1], 2)
-    lms = round(coefficients(lm(lmv ~ seq(1,nrow(df))))[2], 2)
-    lmr = round(summary(lm(lmv~seq(1,nrow(df))))$r.squared, 2)
+    # Define plot name for saving
+    plotname <- paste0(names(df)[t], '.png') #name file
 
-    # 1:1 line coeff
-    i1 = round(min(df[, t])-(max(df[, t])-min(df[, t]))/(nrow(df)-1), 2)
-    s1 = round((max(df[, t])-min(df[, t]))/(nrow(df)-1), 2)
-    s2 = 2*(max(df[, t])-min(df[, t]))/(nrow(df)-1)
+    # Select colors
+    clr <- pal[t-2]
 
-    # Open the png quartz image
-    png(file.path(outdir,
-                  paste0(names(df[t]),'.png')),
-        width = 12, height = 9, units = 'in', res = 180)
+    # Calculate 1:1 line coefficients
+    i1 <- round(min(df[ , t])-(max(df[ , t])-min(df[ , t]))/(nrow(df)-1), 2)
+    s1 <- round((max(df[ , t])-min(df[ , t]))/(nrow(df)-1), 2)
+    s2 <- 2*(max(df[ , t])-min(df[ , t]))/(nrow(df)-1)
 
-    # Print the plot to png
-    print(
-      ggplot(df, aes(x = reorder(Location_ID,  df[, t]), y = df[, t])) +
-        geom_point(aes(color = Established), size = 5) +
-        # scale_color_viridis_c(
-        #   name='Elevation',
-        #   limits=c(
-        #     min(df$Elevation_m),
-        #     max(df$Elevation_m)),
-        #   breaks=c(
-        #     round(min(df$Elevation_m),0),
-        #     round(max(df$Elevation_m))),0) +
-        # scale_color_manual(values = c(clr, 'grey 70', 'grey 30')) +
-        scale_color_manual(values = c(clr)) +
-        scale_y_continuous(name = varname) +
-        labs(x = 'Plot ID', y = names(topos)[t]) +
-        theme_light(base_size = 20) +
-        theme(axis.text.x = element_text(angle = 90, hjust = 1),
-              #legend.position='bottom',
-              legend.position='none',
-              legend.title = element_blank(),
-              legend.key.size = unit(1, 'cm')) +
-        guides(scale = 'none',
-               fill=guide_legend(title="New Legend Title")) +
-        geom_abline(intercept=i1,
-                    slope=s1,
-                    color='black') +
-        geom_abline(intercept=lmi,
-                    slope=lms,
-                    linetype='dashed',
-                    color='black') +
-        geom_text(x=nrow(df),
-                  y=min(df[, t]),
-                  label=as.expression(
-                    substitute(
-                      italic(r)^2~"="~lmr)),
-                  hjust=1) +
-        geom_text(x=nrow(df),
-                  y=min(df[, t])+s1,
-                  label=as.expression(
-                    substitute(
-                      bold('linreg:')~italic(y)~"="~lmi~"+"~lms*italic(x))),
-                  hjust=1) +
-        geom_text(x=nrow(df),
-                  y=min(df[, t])+s2,
-                  label=as.expression(
-                    substitute(
-                      bold('ideal:')~italic(y)~"="~i1~"+"~s1*italic(x))),
-                  hjust=1))
+    # Generate plot
+    ggp <- ggplot(df, aes(x = reorder(Location_ID,  df[, t]), y = df[, t]))
 
+    # Conditional color handling
+    if (missing(col.on)) {
+      ggp <- ggp + geom_point(aes(fill=Factor_Var), size=5, shape=21)
+      if (missing(pal)) {
+        ggp <- ggp + scale_fill_manual(
+          values=c('red2', 'navyblue'),
+          name=factor.var)
+      } else {
+        ggp <- ggp + scale_fill_manual(
+          values=c(clr, 'grey70'),
+          name=factor.var)
+      }
+    } else {
+      ggp <- ggp + geom_point(aes(fill=df[ , col.on]), size=5, shape=21)
+      ggp <- ggp + scale_fill_viridis(name=col.on)
+    }
 
+    ggp <- ggp +
+
+      # Smoothing, 1:1 line and equations
+      geom_smooth(
+        aes(
+          x=1:25,
+          y=df[order(df[,t]), names(df)[t]]),
+        method = 'lm',
+        formula= y~x,
+        se=FALSE,
+        color='black',
+        linewidth=0.5) +
+      geom_abline(
+        intercept=i1,
+        slope=s1,
+        color='black',
+        linetype='dashed') +
+      stat_poly_eq(
+        aes(
+          x=1:25,
+          y=df[order(df[,t]), names(df)[t]],
+          label = paste(after_stat(eq.label), sep = "~~~")),
+        label.x.npc = "right",
+        label.y.npc = 0.15,
+        eq.with.lhs = "italic(hat(y))~`=`~",
+        eq.x.rhs = "~italic(x)",
+        formula = y~x ,
+        parse = TRUE,
+        size = 5) +
+      stat_poly_eq(
+        aes(
+          x=1:25,
+          y=df[order(df[,t]), names(df)[t]],
+          label = paste(after_stat(rr.label), sep = "~~~")),
+        label.x.npc = "right",
+        label.y.npc = "bottom",
+        formula = y~x,
+        parse = TRUE,
+        size = 5) +
+
+      # Formatting
+      scale_y_continuous(name = names(df)[t]) +
+      labs(x = 'Plot ID', y = names(df)[t]) +
+      theme_light(base_size = 20) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1),
+            legend.position='bottom')
+
+    # Print plot
+    png(
+      file.path(outdir, plotname),
+      width = dims[1],
+      height = dims[2],
+      units = 'px',
+      res = reso)
+    print(ggp)
     dev.off()
   }
 }
